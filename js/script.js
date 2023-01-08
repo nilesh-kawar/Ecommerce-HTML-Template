00b3343c0b7682e442bc74b02eb130f18d91a0af
Nilesh Kawar <nilesh227kawar@gmail.com>
2023-01-08 22:39:19 +0530

    final changes

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    function openNav() {
+    document.getElementById("mySidenav").style.width = "70%";
+    
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
+    
+    function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "rgba(0,0,0,0)";
+    }
+    
+    
+    });
